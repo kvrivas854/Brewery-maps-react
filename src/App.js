022948c72e69ca95d91759156555986e7cc09366
Kelly Rivas <kvrivas854@mail.cfcc.com>
2021-07-06 17:38:26 -0400

    Brewery API added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import SearchForm from './components/SearchForm.js'
 
 function App() {
+  const [breweries, setBreweries] = useState('');
+  const [location, setLocation] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState('true');
+
+  useEffect(() => {
+      fetch('https://api.openbrewerydb.org/breweries')
+      .then(response => {
+        if (response.ok) {
+          return response.json()
+        }
+        throw response;
+      })
+      .then(breweries => {
+        setBreweries(breweries)
+        console.log(breweries)
+      })
+      .catch(error => {
+        console.error("error fetching data: ", error);
+        setError(error); 
+      })
+      .finally(() => {
+        setLoading(false);
+      })
+    }, []); 
+  
+    
+  
+
+
+
   return (
     <div>
       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
